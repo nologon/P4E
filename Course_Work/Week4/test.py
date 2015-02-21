22e95b7deedf3eaf8863453bf37d1f5e0a0accf7
Thijn van der Schoot <thijn@tripledotdiving.nl>
2015-02-21 15:53:30 +0100

    New files

---
+++ Course_Work/Week4/test.py
@@ -0,0 +1,10 @@
+__author__ = 'thijn'
+
+h = 100
+if h > 40:
+    print "Working hard"
+    #return h
+        #return 42.37
+elif h <= 40:
+    print "what the fuck"
+    #return h
---
+++ Course_Work/Week4/tets.py
@@ -0,0 +1,13 @@
+__author__ = 'thijn'
+def func(x) :
+    print x
+
+func(10)
+func(20)
+
+def addtwo(a, b):
+    added = a + b
+    return a
+
+x = addtwo(2, 7)
+print x
